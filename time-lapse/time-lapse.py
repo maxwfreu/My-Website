2c8955a9c658d03c52d255ae0d132d264c8be71c
maxwfreu\n <maxwfreu@alumni.stanford.edu>
2019-08-09 04:01:20 +0000

    Terrarium Image Upload

--- time-lapse/time-lapse.py
+++ time-lapse/time-lapse.py
@@ -1,4 +1,5 @@
 from time import sleep
+from git import Repo
 import picamera
 import os
 
@@ -6,6 +7,8 @@ WAIT_TIME = 30 # In Seconds
 GIT_ADD = 'git add . '
 GIT_COMMIT = "git commit -m 'Terrarium Image Upload'"
 GIT_PUSH = 'git push'
+PATH_OF_GIT_REPO = '/home/pi/projects/My-Website/.git'
+COMMIT_MSG = 'Terrarium Image Upload'
 
 with picamera.PiCamera() as camera:
     camera.resolution = (1024, 768)
@@ -14,8 +17,13 @@ with picamera.PiCamera() as camera:
         format='jpeg',
         quality=100,
     )
-    os.system(GIT_ADD)
-    os.system(GIT_COMMIT)
-    os.system(GIT_PUSH)
+    try:
+        repo = Repo(PATH_OF_GIT_REPO)
+        repo.git.add(update=True)
+        repo.index.commit(COMMIT_MSG)
+        origin = repo.remote(name='origin')
+        origin.push()
+    except:
+        print('Big yikes. I bet future you wishes this was a helpful error message. Too bad.')
     #for filename in camera.capture_continuous('/home/pi/projects/time-lapse/img{timestamp:%H-%M-%S-%f}.jpg'):
      #   sleep(WAIT_TIME)
