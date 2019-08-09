42877fed1b659a81f5a76b047eaf7c08260800f5
maxwfreu <maxwfreu@alumni.stanford.edu>
2019-08-09 03:53:17 +0000

    Terrarium Image Upload

--- time-lapse/time-lapse.py
+++ time-lapse/time-lapse.py
@@ -1,15 +1,21 @@
 from time import sleep
 import picamera
-
+import os
 
 WAIT_TIME = 30 # In Seconds
+GIT_ADD = 'git add . '
+GIT_COMMIT = "git commit -m 'Terrarium Image Upload'"
+GIT_PUSH = 'git push'
 
 with picamera.PiCamera() as camera:
     camera.resolution = (1024, 768)
     camera.capture(
-        '../static/images/time-lapse/img{timestamp:%H-%M-%S-%f}.jpeg',
+        '../static/images/time-lapse/terrarium.jpeg',
         format='jpeg',
         quality=100,
     )
+    os.system(GIT_ADD)
+    os.system(GIT_COMMIT)
+    os.system(GIT_PUSH)
     #for filename in camera.capture_continuous('/home/pi/projects/time-lapse/img{timestamp:%H-%M-%S-%f}.jpg'):
      #   sleep(WAIT_TIME)
