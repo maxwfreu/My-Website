f74622fba85ffd1e0e7a821f5e4dfc2d40e7230b
Max Frundlich <maxwfreu@alumni.stanford.edu>
2019-08-10 19:29:54 -0700

    add apple touch icon

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,8 @@ export default class MyDocument extends Document {
           <meta name="theme-color" content="#121212" />
           <link rel="manifest" href="/static/manifest.json" />
           <link rel="Site Icon" href="/static/images/logo/16.ico" />
+          <link rel="icon" sizes="192x192" href="/static/images/logo/192.png" />
+          <link rel="apple-touch-icon" href="ios-icon.png" />
           <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic&display=swap" rel="stylesheet" />
           <meta name="google-site-verification" content="yb_-bhUcnoxh94uAQhPLDK-a1Gt7a9KHiZsGEHYXwqI" />
         </Head>
