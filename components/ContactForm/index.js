6e54267b1dc9a1ed98de1aeb5c3e6d2dc4a8690a
Max Frundlich <maxwfreu@alumni.stanford.edu>
2019-08-31 14:12:18 -0700

    Fixing submission

--- components/ContactForm/index.js
+++ components/ContactForm/index.js
@@ -1,34 +1,59 @@
 import React, { useState } from 'react';
 import '../../static/styles/contact-form.scss';
 
-const ContactForm = () => {
-  const [didContact, setDidContact] = useState(false);
-  if (didContact) {
-    return (
-      <div>
-        <p>Message Sent! 🎉</p>
-        Thanks for reaching out. I'll get back to you as soon as I can :)
-      </div>
-    )
+
+export default class ContactForm extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showSuccess: false,
+    }
   }
-  return (
-    <form className="contact-form" name="contact" method="POST" netlify-honeypot="gotcha" data-netlify="true">
-      <div className="gotcha">
-        <label>Don’t fill this out if you're human: <input name="gotcha" /></label>
-      </div>
-      <div>
-        <label>Name <input type="text" name="name" required /></label>
-      </div>
-      <div>
-        <label>Email <input type="email" name="email" required /></label>
-      </div>
-      <div>
-        <label>Message: <textarea name="message" required></textarea></label>
-      </div>
-      <div>
-        <button type="submit" onSubmit={() => setDidContact(true)}>Send</button>
-      </div>
-    </form>
-  );
+
+  componentDidMount() {
+    const { search } = window.location;
+    if (search.indexOf('success=1') > 0) {
+      this.setState({
+        showSuccess: true,
+      });
+    }
+  }
+
+  render() {
+    if (this.state.showSuccess) {
+      return (
+        <div>
+          <p>Message Sent! 🎉</p>
+          Thanks for reaching out. I'll get back to you as soon as I can :)
+        </div>
+      );
+    }
+
+    return (
+      <form
+        className="contact-form"
+        name="contact"
+        method="POST"
+        netlify-honeypot="gotcha"
+        data-netlify="true"
+        actionURL="/?success=1"
+      >
+        <div className="gotcha">
+          <label>Don’t fill this out if you're human: <input name="gotcha" /></label>
+        </div>
+        <div>
+          <label>Name <input type="text" name="name" required /></label>
+        </div>
+        <div>
+          <label>Email <input type="email" name="email" required /></label>
+        </div>
+        <div>
+          <label>Message: <textarea name="message" required></textarea></label>
+        </div>
+        <div>
+          <button type="submit">Send</button>
+        </div>
+      </form>
+    );
+  }
 }
-export default ContactForm;
