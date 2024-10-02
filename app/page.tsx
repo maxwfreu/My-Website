05848d3b7ff6542d90feb74975c045b3107e7b65
Max Freundlich <mfreundlich1@gmail.com>
2024-10-02 16:02:00 -0400

    feat: link socials

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import { CodeIcon, MailIcon } from "lucide-react";
+import { MailIcon } from "lucide-react";
 import { Metadata } from "next";
+import { SiGithub, SiLinkedin, SiX } from "@icons-pack/react-simple-icons";
 
 export const metadata: Metadata = {
   title: "Max Freundlich",
@@ -12,53 +13,57 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className="bg-blackfont-montserrat text-white overflow-hidden">
-      <main id="top">
-        <div className="h-screen flex justify-center items-center overflow-hidden px-4 pb-8">
-          <div className="max-w-md mx-auto space-y-4 lg:max-w-5xl lg:grid lg:grid-cols-2 lg:gap-5 lg:space-y-0">
-            <div>
-              <h1 className="text-3xl md:text-6xl">
-                Hi, I&apos;m <span className="text-blue-400">Max</span>
-              </h1>
-              <div className="text-sm md:text-base mt-8">
-                <p>
-                  I&apos;m a software developer in Philadelphia, PA. I like
-                  building websites.
-                </p>
-                <p>
-                  I&apos;m currently a Software Engineer at{" "}
-                  <a
-                    href="https://everpilot.com"
-                    target="_blank"
-                    className="text-blue-400 hover:underline"
-                  >
-                    Everpilot
-                  </a>
-                  .
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col">
+      <main className="h-screen flex justify-center items-center overflow-hidden px-4">
+        <div className="flex flex-col gap-4">
+          <h1 className="text-3xl md:text-6xl">
+            Hi, I&apos;m <span className="text-blue-400">Max</span>
+          </h1>
+          <div className="text-sm md:text-base">
+            <p>
+              I&apos;m a software developer in Philadelphia, PA. I like building
+              websites.
+            </p>
+            <p>
+              I&apos;m currently a Software Engineer at{" "}
               <a
-                href="mailto:mfreundlich1@gmail.com"
-                className="hover:underline flex items-center min-h-[48px]"
+                href="https://everpilot.com"
+                target="_blank"
+                className="text-blue-400 hover:underline"
               >
-                <div className="w-6 md:w-12">
-                  <MailIcon className="w-6 md:w-12" />
-                </div>
-                <span className="text-sm md:text-base ml-4">Email me</span>
+                Everpilot
               </a>
-              <a
-                href="https://github.com/maxwfreu"
-                className="hover:underline flex items-center min-h-[48px]"
-              >
-                <div className="w-6 md:w-12">
-                  <CodeIcon className="w-6 md:w-12" />
-                </div>
-                <span className=" text-sm md:text-base ml-4">
-                  Follow me on Github
-                </span>
-              </a>
-            </div>
+              .
+            </p>
+          </div>
+          <div className="flex gap-4 flex-wrap">
+            <a
+              href="https://github.com/maxwfreu"
+              className="group"
+              target="_blank"
+            >
+              <SiGithub size={20} className="group-hover:fill-blue-400" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/max-freundlich-0691a5236/"
+              className="group"
+              target="_blank"
+            >
+              <SiLinkedin size={20} className="group-hover:fill-blue-400" />
+            </a>
+            <a
+              href="https://x.com/MaximumCodeage"
+              className="group"
+              target="_blank"
+            >
+              <SiX size={20} className="group-hover:fill-blue-400" />
+            </a>
+            <a
+              href="mailto:mfreundlich1@gmail.com"
+              className="group"
+              target="_blank"
+            >
+              <MailIcon size={20} className="group-hover:text-blue-400" />
+            </a>
           </div>
         </div>
       </main>
